553ee455162536b3e0a064676f978a710362c7d5
Shivish468 <shivamaryan468@gmail.com>
2023-08-27 00:06:59 +0530

    initial commit

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,16 @@ const app = express();
 const authRouter = require('./router/authRoute');
 const databaseconnect = require('./config/databaseConfig');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 databaseconnect();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: [process.env.CLIENT_URL],
+    credentials: true
+}))
 
 app.use('/api/auth/', authRouter);
 app.use('/', (req, res) => {
--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,5 +1,7 @@
 const userModel = require("../model/userSchema");
 const emailValidator = require("email-validator");
+const bcrypt = require('bcrypt');
+
 const signup = async (req, res, next) => {
     const {name, email, password, confirmPassword} = req.body;
     console.log(name, email, password, confirmPassword);
@@ -65,7 +67,7 @@ const signin = async (req, res) => {
             email
         })
         .select('+password');
-        if(!user || user.password !== password){
+        if(!user || !(await bcrypt.compare(password,user.password))){
             return res.status(400).json({
                 success :false ,
                 message: "Invalid credentials"
